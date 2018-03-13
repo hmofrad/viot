f17d11daa0e4acf21a1ac48617dbe562f625dd63
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2018-03-13 04:36:35 +0000

    from pi

--- connector.py
+++ connector.py
@@ -5,7 +5,13 @@
 
 
 import speech_recognition as sr
- 
+from subprocess import call
+import os
+
+
+keywords = ['play', 'start', 'stop']
+INVALID_CMD = True
+
 # Record Audio
 r = sr.Recognizer()
 with sr.Microphone() as source:
@@ -17,7 +23,22 @@ try:
     # for testing purposes, we're just using the default API key
     # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
     # instead of `r.recognize_google(audio)`
-    print("You said: " + r.recognize_google(audio))
+    transcribed_text = r.recognize_google(audio)
+    
+    print("You said: " + transcribed_text)
+    for key in keywords: 
+        if key in transcribed_text:
+            command = key
+            if(key == 'play'):
+                command = 'start'
+            INVALID_CMD = False
+            print(command)
+            break
+            
+    if(INVALID_CMD == True):
+        print('Unsupported command')
+    else:
+        os.system('python3.5 player.py mpg123 ' + command)
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
