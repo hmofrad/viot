6c6ac9659f6a135582b50dd3aa79775b33d9c90d
root <m.hasanzadeh.mofrad@gmail.com>
2018-03-27 01:07:13 +0000

    new

---
+++ mplayer.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+#echo "Hello,wolrd"
+
+rm -rf /home/pi/viota/mplayer.fifo
+sleep 1
+mkfifo /home/pi/viota/mplayer.fifo
+mplayer -shuffle -idle -slave -input file=/home/pi/viota/mplayer.fifo /home/pi/Music/* 1&>/dev/null &
+sleep 1
+echo 'pause' >  /home/pi/viota/mplayer.fifo
+
+python3.5 /home/pi/viota/connector.py > /home/pi/viota/speech_logs 2>&1 &
