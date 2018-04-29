c91d8200de424b7a79361afc2c7705101e4181ba
Debashis Ganguly <m.hasanzadeh.mofrad@gmail.com>
2018-04-29 19:20:59 -0400

    Update player.sh

---
+++ src/pi/player.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+# Speech API startup service
+# (c) Mohammad Mofrad, 2018
+# (e) mohammad.hmofrad@pitt.edu
+
+
+rm -rf /home/pi/viota/mplayer.fifo
+sleep 1
+mkfifo /home/pi/viota/mplayer.fifo
+mplayer -shuffle -idle -slave -input file=/home/pi/viota/mplayer.fifo /home/pi/Music/* 1&>/dev/null &
+sleep 1
+echo 'pause' >  /home/pi/viota/mplayer.fifo
+
+python3.5 /home/pi/viota/connector.py > /home/pi/viota/speech_logs 2>&1 &
